81f9229525299d693a66f94bf874568fdfa0c758
Sobserius <151635095+Sobserius@users.noreply.github.com>
2026-03-21 19:17:25 +1000

    Update keyboard-layout.sh

--- waybar/scripts/keyboard-layout.sh
+++ waybar/scripts/keyboard-layout.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mmsg -g -k 2>/dev/null | awk '{print $NF}' | tr '[:lower:]' '[:upper:]' || echo "US"
-mmsg -w -k 2>/dev/null | while read -r; do
-    mmsg -g -k 2>/dev/null | awk '{print $NF}' | tr '[:lower:]' '[:upper:]' || echo "US"
+mmsg -g -w -k | while read -r LAYOUT; do
+    if [ -n "$LAYOUT" ]; then
+        echo "{\"text\": \"${LAYOUT^^}\", \"class\": \"$LAYOUT\"}"
+    fi
 done
