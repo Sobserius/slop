52e8b299845bb7270ab11934c60dc75f2913426c
Sobserius <151635095+Sobserius@users.noreply.github.com>
2026-03-21 18:39:06 +1000

    Update keyboard-layout.sh

--- waybar/scripts/keyboard-layout.sh
+++ waybar/scripts/keyboard-layout.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-mmsg -g -k 2>/dev/null | awk '{print $NF}' | tr '[:lower:]' '[:upper:]' || echo "US"
+[[ "$1" == "--watch" ]] || { "$0" --watch & exit; }
+[[ -p /dev/stdin ]] && exec cat >/dev/null
+while sleep 1; do
+    cur=$(mmsg -g -k 2>/dev/null | awk '{print $NF}' | tr '[:lower:]' '[:upper:]')
+    [[ "$cur" != "$prev" ]] && prev="$cur"
+done
